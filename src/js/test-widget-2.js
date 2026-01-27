6966532ee2e5c7a3f5dfe40690920eecc9c15fe8
Roj Bedana <rbedana@ylopo.com>
2026-01-27 16:39:09 +0800

    24

--- src/js/test-widget-2.js
+++ src/js/test-widget-2.js
@@ -76,6 +76,42 @@
           </div>
         </div>
       </div>`);
+    const $minPriceSelector = $(`
+      <div class="search-config-field priceSelector">
+        <label class="ylopo-hidden-label" for="min-price">Min Price</label>
+        <select id="min-price" class="search-config-item">
+          <option value="">Min Price</option>
+          <option value="10000">$10,000</option>
+          <option value="20000">$20,000</option>
+          <option value="30000">$30,000</option>
+          <option value="40000">$40,000</option>
+          <option value="50000">$50,000</option>
+          <option value="60000">$60,000</option>
+          <option value="70000">$70,000</option>
+          <option value="80000">$80,000</option>
+          <option value="90000">$90,000</option>
+          <option value="100000">$100,000</option>
+        </select>
+      </div>`);
+    const $maxPriceSelector = $(`
+      <div class="search-config-field priceSelector">
+        <label class="ylopo-hidden-label" for="max-price">Max Price</label>
+        <select id="max-price" class="search-config-item">
+          <option value="">Max Price</option>
+          <option value="100000">$100,000</option>
+          <option value="200000">$200,000</option>
+          <option value="300000">$300,000</option>
+          <option value="400000">$400,000</option>
+          <option value="500000">$500,000</option>
+          <option value="600000">$600,000</option>
+          <option value="700000">$700,000</option>
+          <option value="800000">$800,000</option>
+          <option value="900000">$900,000</option>
+          <option value="1000000">$1,000,000</option>
+        </select>
+      </div>`);
+    $v5SearchWidget.find('#search-filters').append($minPriceSelector);
+    $v5SearchWidget.find('#search-filters').append($maxPriceSelector);
     $v5SearchWidget.find('#search-filters .propertyTypeSelector').append($propertyTypeSelector);
     $v5SearchWidget.appendTo(container);
   }
